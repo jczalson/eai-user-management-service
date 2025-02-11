68764f11b0d1ef54966f22d8f9a3ac7c3f69aec8
JC Zale <jczalson@gmail.com>
2025-02-11 14:37:37 +0100

    changes done

--- src/main/java/com/eai/user/Application.java
+++ src/main/java/com/eai/user/Application.java
@@ -1,15 +1,27 @@
 package com.eai.user;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import com.eai.user.configuration.ApplicationConfig;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableConfigurationProperties(ApplicationConfig.class)
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
+CommandLineRunner runner = new CommandLineRunner() {
+	
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+};
 }
--- src/test/java/com/eai/user/ApplicationTests.java
+++ src/test/java/com/eai/user/ApplicationTests.java
@@ -1,12 +1,19 @@
 package com.eai.user;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@Tag("unit")
 class ApplicationTests {
 
-	@Test
+	/* @Test */
 	void contextLoads() {
 	}
 
