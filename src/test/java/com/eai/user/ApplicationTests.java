db23a47a0f98a7223351b9bb4f670c5d0c227de4
JC Zale <jczale@gmail.com>
2026-04-21 17:21:22 +0200

    Unit test fixed

--- src/test/java/com/eai/user/ApplicationTests.java
+++ src/test/java/com/eai/user/ApplicationTests.java
@@ -4,13 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
-@ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
 @Tag("unit")
 class ApplicationTests {
